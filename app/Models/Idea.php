59d823c25d4b5457bcadde89f8a5567c79d8710c
ar-j-un <arjun.menon@prophaze.com>
2026-05-07 15:39:52 +0530

    feat: add step model and idea relationships
    Create Step model and migration
    Implement belongsTo relationship in Step model
    Add user and steps relationships in Idea model

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -7,6 +7,8 @@ use Database\Factories\IdeaFactory;
 use Illuminate\Database\Eloquent\Casts\AsArrayObject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Idea extends Model
 {
@@ -17,4 +19,14 @@ class Idea extends Model
         'links' => AsArrayObject::class,
         'status' => IdeaStatus::class,
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function steps(): HasMany
+    {
+        return $this->hasMany(Step::class);
+    }
 }

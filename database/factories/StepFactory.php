65722db65f3968965613fefe096c44787a871a78
ar-j-un <arjun.menon@prophaze.com>
2026-05-07 16:47:41 +0530

    feat: add factories and dummy data setup
    Create Idea and Step factories
    Generate dummy data for testing and development
    Verify factory data creation workflow

--- database/factories/StepFactory.php
+++ database/factories/StepFactory.php
@@ -18,7 +18,9 @@ class StepFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'idea_id' => Idea::factory(),
+            'description' => fake()->sentence,
+            'completed' => false,
         ];
     }
 }

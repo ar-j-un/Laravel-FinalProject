bbdc239b046dd43300255e1b658cc645ed2abb82
ar-j-un <arjun.menon@prophaze.com>
2026-05-08 12:57:21 +0530

    feat(ui): add layout and navigation components
    Create layout.blade.php and nav.blade.php reusable components
    
    Implement basic UI for the ideas home page

---
+++ resources/views/components/layout/nav.blade.php
@@ -0,0 +1,10 @@
+<nav class="border-b border-border px-6">
+    <div class="max-w-7x1 ma-auto h-16 flex items-center justify-between">
+        <div>
+            <a href="">Idea</a>
+        </div>
+        <div>
+            right side
+        </div>
+    </div>
+</nav>

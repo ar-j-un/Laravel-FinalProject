f955a6cd71889bdfcddf1afcd32f784b504fb319
ar-j-un <arjun.menon@prophaze.com>
2026-05-11 17:50:49 +0530

    feat(auth): implement user login authentication
    Add POST login route
    Implement store method in SessionController
    Validate login credentials
    Create authenticated user session
    Log in authenticated users
    Add authentication error messages

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class SessionController extends Controller
@@ -16,5 +17,24 @@ class SessionController extends Controller
         Auth::logout();
 
         return redirect('/')->with('success', 'Logged out successfully!');
+    }
+
+    public function store(Request $request)
+    {
+        // dd($request->all());
+        $attributes = $request->validate([
+            'email' => ['required', 'email', 'string', 'max:255'],
+            'password' => ['required', 'string', 'min:6', 'max:255'],
+        ]);
+        if (! Auth::attempt($attributes)) {
+            return back()
+                ->withErrors(['email' => 'We were unable to authenticate using the provided credentials'])
+                ->withInput();
+
+        }
+
+        $request->session()->regenerate();
+
+        return redirect('/')->with('success', 'Logged in successfully!');
     }
 }
